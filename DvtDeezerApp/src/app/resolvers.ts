4e5fdf2159401b9797b700df8758602424cf0d2e
Tinus Greyling <TinusG@automationexchange.co.za>
2021-09-21 12:21:16 +0200

    display album data

--- DvtDeezerApp/src/app/resolvers.ts
+++ DvtDeezerApp/src/app/resolvers.ts
@@ -1,32 +1,32 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { Observable, of } from "rxjs";
-import { ArtistService } from "./root-services/artist.service";
-import { IAlbum, IArtist, IArtistTopTracks } from "./shared/types";
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { ArtistService } from './root-services/artist.service';
+import { IAlbum, IArtist, IArtistTopTracks } from './shared/types';
 
 @Injectable()
 export class GetArtistDetailsResolver implements Resolve<IArtistTopTracks[]> {
 
-	constructor(private readonly artistService: ArtistService) { }
+  constructor(private readonly artistService: ArtistService) { }
 
-	public resolve(route: ActivatedRouteSnapshot): Observable<IArtistTopTracks[]> {
+  public resolve(route: ActivatedRouteSnapshot): Observable<IArtistTopTracks[]> {
     const artistId = route.params.id;
 
     if (!artistId)
       return of(null);
 
     return this.artistService.getArtistTopTracks(artistId);
-	}
+  }
 
 }
 
 @Injectable()
 export class GetArtistResolver implements Resolve<IArtist> {
 
-	constructor(private readonly artistService: ArtistService,
+  constructor(private readonly artistService: ArtistService,
   ) {}
 
-	public resolve(route: ActivatedRouteSnapshot): Observable<IArtist> {
+  public resolve(route: ActivatedRouteSnapshot): Observable<IArtist> {
     const artistId = route.params.id;
 
     if (!artistId)
@@ -41,6 +41,24 @@ export class GetArtistResolver implements Resolve<IArtist> {
     } else {
       return of(artist);
     }
-	}
+  }
 
 }
+
+@Injectable()
+export class GetAlbumResolver implements Resolve<IAlbum[]> {
+
+  constructor(private readonly artistService: ArtistService,
+  ) {}
+
+  public resolve(route: ActivatedRouteSnapshot): Observable<IAlbum[]> {
+    const artistId = route.params.id;
+
+    if (!artistId)
+      return of(null);
+
+    return this.artistService.getAlbums(artistId);
+  }
+
+}
+
